1079881fe054651c69b53a12d4e01fe83352f9f1
jennfrbrown <jennfrbrown@gmail.com>
2020-10-25 18:04:30 -0400

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,9 @@ Suspension_Coil <-read.csv(file="Suspension_Coil.csv")
 total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI)) #create summary table with multiple columns
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+t.test(Suspension_Coil$PSI,mu=1500) #compare sample versus 1500 population mean
+
+t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot=="Lot1"), mu=1500)
+t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot=="Lot2"), mu=1500)
+t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot=="Lot3"), mu=1500)
