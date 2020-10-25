55622b524b5edefded19a888702b01305aa90bb8
Jennifer Brown <jennfrbrown@gmail.com>
2020-10-25 13:42:33 -0500

    adding R script

---
+++ MechCarChallenge.R
@@ -0,0 +1,6 @@
+library(dbplyr)
+
+MechaCar_mpg <-read.csv(file="MechaCar_mpg.csv")
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)) #generate summary statistics
